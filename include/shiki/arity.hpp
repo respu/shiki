14658cd1e56cb53fcfe46b184c3d93d4d424517d
Joel Falcou <joel.falcou@lri.fr>
2015-08-04 13:58:09 +0200

    arity and arity_

---
+++ include/shiki/arity.hpp
@@ -0,0 +1,55 @@
+//==================================================================================================
+/*!
+  @file
+
+  Defines the tag_of function
+
+  @copyright 2015 Joel Falcou
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+
+**/
+//==================================================================================================
+#pragma once
+
+#include <shiki/detail/ignore.hpp>
+#include <shiki/is_terminal.hpp>
+
+namespace shiki
+{
+  template<typename T> struct terminal;
+
+  /*!
+    @ingroup group-api
+    @brief Return the number of children of a given expression
+
+    Retrieves the number of children of a given expression.
+    Note that terminals have an arity equals to 0.
+
+    @param  xpr  Expression to process
+  **/
+  template<typename Expr> inline constexpr auto arity(Expr const& xpr)
+  {
+    return xpr.accept ( [](auto const& tag, auto&&... args)
+                      {
+                        detail::ignore(tag, args...);
+                        return std::integral_constant < std::size_t
+                                                      , is_terminal_<decltype(tag)>::value
+                                                        ? 0u : sizeof...(args)
+                                                      >{};
+                      }
+                    );
+  }
+
+  /*!
+    @ingroup group-meta
+    @brief Return the type of the arity of a given expression
+
+    For any given Expression, returns the arity of the expression.
+
+    @tparam Expr Expression to retrieve the arity from
+  **/
+  template<typename Expr> using arity_ = decltype(arity(std::declval<Expr>()));
+
+}
---
+++ test/arity.cpp
@@ -0,0 +1,54 @@
+//==================================================================================================
+/*
+  Copyright 2015 Joel FALCOU
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#include <shiki/arity.hpp>
+#include <shiki/make_expr.hpp>
+
+#include <nstest.hpp>
+
+// Expression tag
+struct value_ : shiki::tag::terminal_ {};
+struct foo {};
+
+NSTEST_CASE( "Check that terminal expressions have arity of 0")
+{
+  NSTEST_EQUAL( shiki::arity(shiki::make_expr<shiki::tag::terminal_>(1.f)), 0u );
+  NSTEST_EQUAL( shiki::arity(shiki::make_expr<value_>(69u)), 0u );
+
+  NSTEST_TYPE_IS( shiki::arity_<decltype(shiki::make_expr<shiki::tag::terminal_>(1.f))>
+                , (std::integral_constant<std::size_t,0u>)
+                );
+  NSTEST_TYPE_IS( shiki::arity_<decltype(shiki::make_expr<value_>(69u))>
+                , (std::integral_constant<std::size_t,0u>)
+                );
+}
+
+NSTEST_CASE( "Check that random expressions have proper arity")
+{
+  NSTEST_EQUAL( shiki::arity(shiki::make_expr<foo>(42))     , 1u );
+  NSTEST_EQUAL( shiki::arity(shiki::make_expr<foo>(6,9))    , 2u );
+  NSTEST_EQUAL( shiki::arity(shiki::make_expr<foo>(1,'O',1)), 3u );
+  NSTEST_EQUAL( shiki::arity(shiki::make_expr<foo>(1,3,3,7)), 4u );
+  NSTEST_EQUAL( shiki::arity(shiki::make_expr<foo>('h','e','l','l','o',' ','c','+','+','!')), 10u );
+
+  NSTEST_TYPE_IS( (shiki::arity_<decltype(shiki::make_expr<foo>(42))>)
+                , (std::integral_constant<std::size_t,1u>)
+                );
+  NSTEST_TYPE_IS( (shiki::arity_<decltype(shiki::make_expr<foo>(6,9))>)
+                , (std::integral_constant<std::size_t,2u>)
+                );
+  NSTEST_TYPE_IS( (shiki::arity_<decltype(shiki::make_expr<foo>(1,'O',1))>)
+                , (std::integral_constant<std::size_t,3u>)
+                );
+  NSTEST_TYPE_IS( (shiki::arity_<decltype(shiki::make_expr<foo>(1,3,3,7))>)
+                , (std::integral_constant<std::size_t,4u>)
+                );
+  NSTEST_TYPE_IS( (shiki::arity_<decltype(shiki::make_expr<foo>('h','e','l','l','o',' ','c','+','+','!'))>)
+                , (std::integral_constant<std::size_t,10u>)
+                );
+}
